ac714facfb767bbb69afab070c19784d3f75d7f1
Malek <s14114@pjwstk.edu.pl>
2018-04-09 15:54:19 +0200

    First Commit

---
+++ app/src/main/java/com/malek/expensesmanager/ChartsActivity.java
@@ -0,0 +1,8 @@
+package com.malek.expensesmanager;
+
+/**
+ * Created by Małek on 2018-04-09.
+ */
+
+public class ChartsActivity {
+}
---
+++ app/src/main/java/com/malek/expensesmanager/PhotoReceiptActivity.java
@@ -0,0 +1,8 @@
+package com.malek.expensesmanager;
+
+/**
+ * Created by Małek on 2018-04-09.
+ */
+
+public class PhotoReceiptActivity {
+}
